0d5991c312c315bf2554eded1e48586323c7dfaa
caozhiy <1030434805@qq.com>
2023-10-22 17:55:31 +0800

    Add MyAnnotation related files.

---
+++ src/edu/TestAnnotation/MyAnnotation.java
@@ -0,0 +1,12 @@
+package edu.TestAnnotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
+public @interface MyAnnotation {
+    String value() default "注释";
+}
--- src/edu/TestAnnotation/TestAnnotation.java
+++ src/edu/TestAnnotation/TestAnnotation.java
@@ -1,7 +1,35 @@
 package edu.TestAnnotation;
 
+import edu.TestClass.TestClass;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+@MyAnnotation("TestAnnotation for Class")
 public class TestAnnotation {
     public static void main(String[] args) {
+        test();
+    }
+
+    @MyAnnotation("static method annotation")
+    public static void test() {
+
+        // 获取类的注解
+        Class<TestAnnotation> clazz = TestAnnotation.class;
+        MyAnnotation annotation = clazz.getAnnotation(MyAnnotation.class);
+        System.out.println(annotation.value());
+
+
+        // 利用反射机制获取方法的注解
+        try {
+            Method method = clazz.getMethod("test");
+            annotation = method.getAnnotation(MyAnnotation.class);
+            System.out.println(annotation.value());
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
 
     }
+
 }
